71527a49d7e0c52f8d2ee843f09b0dd7dfbf4254
asmii123 <asmimishra123@gmail.com>
2022-03-13 14:04:57 +0545

    Form Update

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,26 +1,37 @@
 // import React from "react"
-import Modal from 'react-bootstrap/Modal'
-import Button from 'react-bootstrap/Button'
-import React, {useState} from 'react'
+import Modal from "react-bootstrap/Modal";
+import Button from "react-bootstrap/Button";
+import Form from "react-bootstrap/Form";
+import React, { useState } from "react";
 // import Form from "./Form.js"
 
 export default function Main() {
-const [show, setShow] = useState(false);
+  const [show, setShow] = useState(false);
 
-const handleClose = () => setShow(false);
-const handleShow = () => setShow(true);
+  const handleClose = () => setShow(false);
+  const handleShow = () => setShow(true);
 
-
-    return (
-        <div>
-        
-        <Modal show={show} onHide={handleClose}>
+  return (
+    <div>
+      <Modal show={show} onHide={handleClose}>
         <Modal.Header closeButton>
-          <Modal.Title>Donation</Modal.Title>
+          <Modal.Title className="donate--header">Donation</Modal.Title>
         </Modal.Header>
-        <Modal.Body>We're grateful for your contribution!
-        </Modal.Body>
-       
+        <Modal.Body>We're grateful for your contribution!</Modal.Body>
+        <Form>
+          <Form.Group className="form--amount" controlId="formAmount">
+            <Form.Label>Amount</Form.Label>
+            <Form.Control type="amount" placeholder="Enter amount to donate" />
+            {/* <Form.Text className="text-muted">
+              We'll never share your email with anyone else.
+            </Form.Text> */}
+          </Form.Group>
+
+          
+          <Form.Group className="from--check" controlId="formBasicCheckbox">
+            <Form.Check type="checkbox" label="I agree to donate" />
+          </Form.Group>
+        </Form>
         <Modal.Footer>
           <Button variant="secondary" onClick={handleClose}>
             Close
@@ -30,27 +41,22 @@ const handleShow = () => setShow(true);
           </Button>
         </Modal.Footer>
       </Modal>
-      
+
       <div className="main--body">
-       <img src="./images/chairty.png"
-            height='700'
-            widht='1600'
-            className="main--image" />
+        <img
+          src="./images/chairty.png"
+          height="700"
+          widht="1600"
+          className="main--image"
+        />
         <main className="main--title">
-            <h2>Join Us</h2>
-            <h3>To Make a Difference!</h3>  
-             <button
-        className="form--button"
-        onClick={handleShow}
-   
-        >
-        Donate
-        </button>
+          <h2>Join Us</h2>
+          <h3>To Make a Difference!</h3>
+          <button className="form--button" onClick={handleShow}>
+            Donate
+          </button>
         </main>
-             </div>
-       
-       </div>    
-   
-      
-    )
+      </div>
+    </div>
+  );
 }
