9ea509b59ea572ba6beeb0d512f627cbaf2f8df6
asmii123 <asmimishra123@gmail.com>
2022-03-14 21:05:25 +0545

    Update firebase.config.js

--- src/firebase/firebase.config.js
+++ src/firebase/firebase.config.js
@@ -1,11 +1,11 @@
 const firebaseConfig = {
-    apiKey: "AIzaSyBRVJgbVfvAMF8GtfqIadQwV3jPMQQ0hjc",
-    authDomain: "demooooo-f1e7d.firebaseapp.com",
-    projectId: "demooooo-f1e7d",
-    storageBucket: "demooooo-f1e7d.appspot.com",
-    messagingSenderId: "295941954870",
-    appId: "1:295941954870:web:0af92bc8a3e757b2c1e797",
-    measurementId: "G-VYVHZCSGGR"
+    apiKey: "",
+    authDomain: "",
+    projectId: "",
+    storageBucket: "",
+    messagingSenderId: "",
+    appId: "",
+    measurementId: ""
   };
   
   export default firebaseConfig;
