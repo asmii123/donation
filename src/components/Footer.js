7d59b567c4a0ee8a4249f9ceedfbfea0c9ef63ad
asmii123 <asmimishra123@gmail.com>
2022-03-14 17:05:20 +0545

    Footer Update

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -4,7 +4,7 @@ export default function Footer() {
     return (
         <div className="footer">
           
-            <h3 className="footer--text">TOGETHER WE CAN</h3>
+            <h3 className="footer--text">TOGETHER WE CAN!</h3>
         </div>
     )
 }
